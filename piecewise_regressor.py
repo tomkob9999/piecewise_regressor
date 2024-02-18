9697e569151cca444f38e4f9ce5bde13923e2e89
tokob9999 <96751911+tomkob9999@users.noreply.github.com>
2024-02-19 07:12:58 +0900

    1.0.5

--- piecewise_regressor.py
+++ piecewise_regressor.py
@@ -46,8 +46,9 @@ class piecewise_regressor:
                 bic = gmm.bic(X)
 #                 print("n", n, "prev_aic", prev_aic, "aic", aic)
 #                 print("n", n, "prev_bic", prev_bic, "bic", bic)
-                num_clusters = n-1
+                num_clusters = n
                 if (use_aic and aic > prev_aic) or (not use_aic and bic > prev_bic):
+                    num_clusters = n-1
                     gmm = prev_gmm
                     break
                 prev_aic = aic
